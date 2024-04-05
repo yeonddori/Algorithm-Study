e7f55f72e4f26c866aae3c3c9fd9e9bc2ffb83f3
yeonddori <lsymiso@naver.com>
2024-04-05 22:47:49 +0900

    :memo: BOJ_9095_1,2,3-더하기
    https://www.acmicpc.net/problem/9095

---
+++ yeonddori/2024-04/BOJ_9095_1,2,3-더하기.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    dp = [0] * max(4, N + 1)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    print(dp[N])
