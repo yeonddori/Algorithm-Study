282bb5e2d154f1e2a33f57160c154fe715be8d39
ban_chan <kimmc3423@naver.com>
2024-04-06 19:45:34 +0900

    :memo: BOJ_16953_A→B

---
+++ banchan01/2024-04/BOJ_16953_A→B.py
@@ -0,0 +1,18 @@
+a,b = map(int, input().split())
+cnt = 1
+
+while(a != b):
+    temp = b
+    if(b%10 == 1):
+        b //= 10
+        cnt += 1
+
+    elif(b%2 == 0):
+        b //= 2
+        cnt += 1
+
+    if(temp == b):
+        cnt = -1
+        break
+
+print(cnt)
