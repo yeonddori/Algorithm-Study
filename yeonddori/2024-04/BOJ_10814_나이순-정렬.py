7f16fda95a9164fc12ea510b9d1a67dd4d23d48a
yeonddori <lsymiso@naver.com>
2024-04-07 23:54:40 +0900

    :memo: BOJ_10814_나이순-정렬
    https://www.acmicpc.net/problem/10814

---
+++ yeonddori/2024-04/BOJ_10814_나이순-정렬.py
@@ -0,0 +1,11 @@
+N = int(input())
+members = []
+
+for _ in range(N):
+    age, name = input().split()
+    members.append((int(age), name))
+
+members.sort(key=lambda x: x[0])
+
+for member in members:
+    print(member[0], member[1])
