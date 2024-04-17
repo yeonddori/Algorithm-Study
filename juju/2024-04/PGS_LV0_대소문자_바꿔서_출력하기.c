4e4047263cb9b3b91b7cdef6b32dc33bc4ebf166
juju <jujunoidea@gmail.com>
2024-04-17 18:42:58 +0900

    :memo: PGS_LV0_대소문자_바꿔서_출력하기

---
+++ juju/2024-04/PGS_LV0_대소문자_바꿔서_출력하기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define LEN_INPUT 10
+
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+    int i = 0;
+    while(s1[i]){
+        if(isupper(s1[i])){
+            s1[i] = tolower(s1[i]);
+        }
+        else if(islower(s1[i])){
+            s1[i] = toupper(s1[i]);
+        }
+        i++;
+    }
+    printf(s1);
+
+    return 0;
+}
