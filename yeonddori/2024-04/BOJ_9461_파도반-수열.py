58b5d1feea4347491436a6e239b6fa2e6c07dad3
yeonddori <lsymiso@naver.com>
2024-04-19 11:52:26 +0900

    :memo: BOJ_9461_파도반-수열
    https://www.acmicpc.net/problem/9461

---
+++ yeonddori/2024-04/BOJ_9461_파도반-수열.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    dp = [0] * (N + 4)
+    dp[1] = dp[2] = dp[3] = 1
+
+    for i in range(4, N + 1):
+        dp[i] = dp[i - 2] + dp[i - 3]
+
+    print(dp[N])
