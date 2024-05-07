2d6a806570f6ff1174ec30be7d896ab4d430b2ad
suyoung <suyoung.dev@gmail.com>
2024-05-08 01:02:57 +0900

    :memo: BOJ_1309_동물원

--- suyoungee/2024-05/BOJ_1309_동물원.py
+++ suyoungee/2024-05/BOJ_1309_동물원.py
@@ -1,11 +1,17 @@
-from sys import stdin
-N= int(stdin.readline())
+import sys
+input = sys.stdin.readline
+n = int(input())
+dp = [0]*(n+1)
+for i in range(n+1) :
+    dp[i] = [0,0,0]
+dp[1][0] = 1
+dp[1][1] = 1
+dp[1][2] = 1
 
-if N == 1 :
-    print(3)
-else :
-    dp = [1 for _ in range(N+1)]
-    dp[1], dp[2] = 3,7
-    for i in range(3, N+1) :
-        dp[i] = (2 * dp[i-1] + dp[i-2] % 9901)
-    print(dp[N])
+
+for i in range(2, n + 1):
+    dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901
+    dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901
+    dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901
+
+print(sum(dp[n]) % 9901)
