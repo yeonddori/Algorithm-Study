0a6e3ab4940316a9aa2a629978b4a11d2a99a500
ban_chan <kimmc3423@naver.com>
2024-05-13 10:05:45 +0900

    :memo: BOJ_1932_정수삼각형

---
+++ banchan01/2024-05/BOJ_1932_정수삼각형.py
@@ -0,0 +1,21 @@
+n = int(input())
+lst = []
+for _ in range(n):
+    lst.append(list(map(int,input().split())))
+
+dp = [[0]*x for x in range(1,n+1)]
+
+dp[0][0] = lst[0][0]
+
+for i in range(1,n):
+    for j in range(i+1):
+        if j == 0:
+            dp[i][j] = dp[i-1][j] + lst[i][j]
+
+        elif j == i:
+            dp[i][j] = dp[i-1][j-1] + lst[i][j]
+
+        else:
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + lst[i][j]
+
+print(max(dp[n-1]))
