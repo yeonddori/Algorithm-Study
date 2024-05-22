3adffd9e379cc00a6f03625348c31fbe0d8faf5c
yeonddori <lsymiso@naver.com>
2024-05-22 11:22:54 +0900

    :memo: PGS_LV1_신규-아이디-추천
    https://school.programmers.co.kr/learn/courses/30/lessons/72410?language=python3

---
+++ yeonddori/2024-05/PGS_LV1_신규-아이디-추천.py
@@ -0,0 +1,22 @@
+def solution(new_id):
+    answer = ''
+
+    new_id = new_id.lower()
+    for i in new_id:
+        if i.isalnum() or i in '-_.':
+            answer += i
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    if answer[0] == '.' or answer[-1] == '.':
+        answer = answer.strip('.')
+    if answer == '':
+        answer = 'a'
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    if len(answer) <= 2:
+        while len(answer) < 3:
+            answer += answer[-1]
+
+    return answer
