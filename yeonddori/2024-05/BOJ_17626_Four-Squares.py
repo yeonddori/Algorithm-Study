bee4085ae5d78415e7f4223346f4fa07cc9fe775
yeonddori <lsymiso@naver.com>
2024-05-27 23:20:01 +0900

    :memo: BOJ_17626_Four-Squares
    https://www.acmicpc.net/problem/17626

---
+++ yeonddori/2024-05/BOJ_17626_Four-Squares.py
@@ -0,0 +1,12 @@
+n = int(input())
+dp = [0] * (n + 1)
+dp[1] = 1
+
+for i in range(2, n + 1):
+    dp[i] = i
+    j = 1
+    while j * j <= i:
+        dp[i] = min(dp[i], dp[i - j * j] + 1)
+        j += 1
+
+print(dp[n])
