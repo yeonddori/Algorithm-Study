aba8809284fddfc621645181a23d9386140f158d
ban_chan <kimmc3423@naver.com>
2024-06-13 20:39:39 +0900

    memo: BOJ_2493_탑

---
+++ banchan01/2024-06/BOJ_2493_탑.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    stack<pair<int,int>> st;
+    st.push({100000001,0});
+
+    int h;
+    for(int i=1; i<=n; i++)
+    {
+        cin >> h;
+        while(!st.empty())
+        {
+            if (h<=st.top().first)
+            {
+                cout << st.top().second << ' ';
+                break;
+            }
+            else
+            {
+                st.pop();
+            }
+        }
+        st.push(make_pair(h,i));
+    }
+    return 0;
+}
