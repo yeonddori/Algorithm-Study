2cb20938c2c025f8b6544a15fd9b026312141622
dada <131844745+tbvjgjfzm@users.noreply.github.com>
2024-06-23 19:26:30 +0900

    :memo: PGS_LV1_나누어-떨어지는-숫자-배열.py

---
+++ tbvjgjfzm/2024-06/PGS_LV1_나누어-떨어지는-숫자-배열.py
@@ -0,0 +1,9 @@
+def solution(arr, divisor):
+    answer = []
+    for i in range(0, len(arr)):
+        if arr[i] % divisor == 0:
+            answer.append(arr[i])
+    if len(answer) == 0:
+        answer.append(-1)
+    answer = sorted(answer)
+    return answer
