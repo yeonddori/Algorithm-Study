74888a1b0b6fe1089c719933af70b3c8ca116614
ban_chan <kimmc3423@naver.com>
2024-07-04 01:03:12 +0900

    :memo: BOJ_2504_괄호의값

--- banchan01/2024-07/BOJ_2504_괄호의값.cpp
+++ banchan01/2024-07/BOJ_2504_괄호의값.cpp
@@ -7,52 +7,72 @@ int result= 0;
 int tmp = 1;
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     stack<int> st;
 
-    bool close = false;
+    bool open = true;
 
     cin >> s;
     for(char ch :s)
     {
-        if(ch=='(' || ch == '[')
+        if(ch=='(' || ch=='[')
         {
-            if (close)
-            {
-                result += tmp;
-            }
-
             st.push(ch);
-            close = false;
-            tmp = 1;
-        }
-        else if(ch==')')
-        {
-            if(!st.empty() && st.top()=='(')
+            if(ch=='(')
             {
-                st.pop();
                 tmp *= 2;
             }
             else
             {
-                cout << 0;
-                break;
-            }
-            close = true;
-        }
-
-        else if(ch==']')
-        {
-            else if(!st.empty() && st.top()=='[')
-            {
-                st.pop();
                 tmp *= 3;
+            }
+            open = true;
+        }
+        else if(!st.empty() && ch ==')')
+        {
+            if(st.top()== '(')
+            {
+                if(open)
+                {
+                    result += tmp;
+                }
+                st.pop();
+                tmp /= 2;
             }
             else
             {
-                cout << 0;
+                result = 0;
                 break;
             }
-            close = true;
+            open = false;
+        }
+        else if(!st.empty() && ch ==']')
+        {
+            if(st.top()=='[')
+            {
+                if(open)
+                {
+                    result += tmp;
+                }
+                st.pop();
+                tmp /= 3;
+            }
+            else
+            {
+                result = 0;
+                break;
+            }
+            open = false;
+        }
+        else if(st.empty() && (ch==')' || ch ==']'))
+        {
+            result = 0;
+            break;
         }
     }
+    if (!st.empty())
+        result = 0;
+    cout << result<<'\n';
 }
