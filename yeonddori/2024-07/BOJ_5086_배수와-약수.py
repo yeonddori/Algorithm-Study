a36f22b6af7a6eb24f8527b47e704902fa616296
yeonddori <lsymiso@naver.com>
2024-07-15 22:13:45 +0900

    :memo: BOJ_5086_배수와-약수
    https://www.acmicpc.net/problem/5086

---
+++ yeonddori/2024-07/BOJ_5086_배수와-약수.py
@@ -0,0 +1,9 @@
+a, b = map(int, input().split())
+while a != 0 and b != 0:
+    if b % a == 0:
+        print("factor")
+    elif a % b == 0:
+        print("multiple")
+    else:
+        print("neither")
+    a, b = map(int, input().split())
