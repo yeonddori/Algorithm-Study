afe38cf94f36d3472e1dc6776e140c40c02474c3
yeonddori <lsymiso@naver.com>
2024-07-28 23:14:19 +0900

    :memo: BOJ_5543_상근날드
    https://www.acmicpc.net/problem/5543

---
+++ yeonddori/2024-07/BOJ_5543_상근날드.py
@@ -0,0 +1,7 @@
+sangdeok = int(input())
+jungdeok = int(input())
+hadeok = int(input())
+coke = int(input())
+sprite = int(input())
+
+print(min(sangdeok, jungdeok, hadeok) + min(coke, sprite) - 50)
