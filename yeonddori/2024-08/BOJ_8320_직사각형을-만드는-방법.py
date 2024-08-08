1bad568874b04e2752563875bff869a4b9444cd1
yeonddori <lsymiso@naver.com>
2024-08-08 22:30:00 +0900

    :memo: BOJ_8320_직사각형을-만드는-방법
    https://www.acmicpc.net/problem/8320

---
+++ yeonddori/2024-08/BOJ_8320_직사각형을-만드는-방법.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+cnt = 0
+
+for i in range(1, n + 1):
+    for j in range(i, n + 1):
+        if i * j <= n:
+            cnt += 1
+        else:
+            break
+
+print(cnt)
